a2013b073ab7d3feab632d212ffb28fed82c2b6e
RASHA2702 <82314271+RASHA2702@users.noreply.github.com>
2021-09-13 22:59:26 +0530

    assignment completed

---
+++ assignment day 1.py
@@ -0,0 +1,20 @@
+"""1"""
+"""float"""
+a = 6.67
+a is 6.67"""output=syntax warning and false"""
+"""string"""
+b = 'universe'
+b is 'universe' """output=syntax warning and true"""
+"""2"""
+1>3>4
+"""checks from left to right on the condition example if 1>3 is false then it changes to false 3> 4 which is also false so the answer is false"""
+"""3"""
+"""int"""
+c = 5
+int(c) """output = 5"""
+"""float"""
+d = 5.88
+float(d)"""output = 5.88"""
+"""string"""
+e = 'python'
+str(e)"""output = 'python'"""
